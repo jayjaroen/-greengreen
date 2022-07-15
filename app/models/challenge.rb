a686c5a2447e809826d08261b40f0af79d1ba0d0
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-07-15 21:38:15 +0800

    finished the models

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,3 +1,5 @@
 class Challenge < ApplicationRecord
   belongs_to :challenge_category
+  has_many :user_challenges
+  has_many :users, through: :user_challenges
 end
--- app/models/challenge_category.rb
+++ app/models/challenge_category.rb
@@ -1,2 +1,4 @@
 class ChallengeCategory < ApplicationRecord
+  has_many :tips
+  has_many :rankings
 end
