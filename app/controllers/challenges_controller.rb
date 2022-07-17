fbf14a9d29cade51e62cbf272d4d90a3a13fcc86
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-07-17 18:47:08 +0800

    add admin column to user, pundit for challenge_categories & tips

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,13 +4,31 @@ class ChallengesController < ApplicationController
   def index
     @challenge_category = ChallengeCategory.find(params[:challenge_category_id])
     @challenges = @challenge_category.challenges
+    @challenge = policy_scope(Challenge).order(created_at: :desc)
   end
 
   def show
   end
 
+  def new
+    @challenge = Challenge.new
+    authorize @challenge
+  end
+
+  def create
+    # @challenge_category = ChallengeCategory.find(params[:challenge_category_id])
+    @challenge = Challenge.new(challenge_params)
+      if @challenge.save
+        render 'index'
+      end
+  end
+
   private
   def find_challenge_params
     @challenge = Challenge.find(params[:id])
+  end
+
+  def challenge_params
+    params.require(:challenge).permit(:action, :carbon_score, :point, :challenge_category_id)
   end
 end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -3,9 +3,23 @@ before_action :find_tip, only: :show
 
   def index
     @tips = Tip.all
+    @tips = policy_scope(Tip).order(created_at: :desc)
   end
 
   def show
+    authorize @tip
+  end
+
+  def new
+    @tip = Tip.new
+    authorize @tip
+  end
+
+  def create
+    @tip = Tip.new(tip_params)
+    if @tip.save
+      redirect_to tips_path
+    end
   end
 
   private
