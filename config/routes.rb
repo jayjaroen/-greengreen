62fb0b03d8ddd7d7aadb64e73d6540b4bac07980
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-07-18 21:20:04 +0800

    link to profile page works

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "pages#landing"
+  get 'pages/home', to: 'pages#home', as: 'home'
   get 'users/:id/profile', to: 'users#profile', as: 'profile'
 
 
