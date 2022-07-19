ba1c4ce435a72d340d0f8f3c3e8db52f835bb261
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-07-20 00:02:39 +0800

    user can add the challenge

--- app/controllers/user_challenges_controller.rb
+++ app/controllers/user_challenges_controller.rb
@@ -1,2 +1,19 @@
 class UserChallengesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @challenge = Challenge.find(params[:challenge_id])
+    @user_challenge = UserChallenge.new(user_challenge_params)
+    authorize @user_challenge
+    @user_challenge.user = current_user
+    redirect_to profile_path(@user_challenge.user) if @user_challenge.save
+  end
+
+  private
+
+  def user_challenge_params
+    params.require(:user_challenge).permit(:user_id, :challenge_id, :total_score)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_many :user_challenges
   has_many :rankings
   has_many :challenges, through: :user_challenges
+
+  def total_carbon_score
+    total = Challenge.all.map do |challenge|
+      # no dryer
+      # 100 * 5
+      next if challenge.carbon_score.nil?
+      subtotal = challenge.carbon_score * user_challenges.joins(:user_challenge_records).where(challenge_id: challenge.id).count
+    end
+    # [0, 100, 400, 500, 0]
+    # total = total.sum
+    total = total.reject{|x| x.nil?}.sum
+  end
 end
