ec3914e8baaa59cb55e42b7dd9e47a1caaebdab3
Lisa Yang <lisayang041@gmail.com>
2022-07-23 13:34:28 +0800

    navbar dropdown

---
+++ app/javascript/scripts/toggle-navbar.js
@@ -0,0 +1,12 @@
+const toggleNavbar = () => {
+  var navbar = document.getElementById("navbar")
+  const button = document.getElementById('navbar-toggle')
+  if (navbar && button) {
+    button.addEventListener('click', (e) => [
+      navbar.classList.toggle("hidden")
+    ])
+
+  }
+}
+
+export { toggleNavbar }
