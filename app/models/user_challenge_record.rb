601f516b3c5b63de8ed0bf2406935331cfab2511
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-08-02 22:08:55 +0800

    chartkick profile page

--- app/models/user_challenge_record.rb
+++ app/models/user_challenge_record.rb
@@ -1,6 +1,12 @@
 class UserChallengeRecord < ApplicationRecord
   belongs_to :user_challenge
+  has_one :user, through: :user_challenge
+  has_one :challenge, through: :user_challenge
   after_create :calculate_category_score
+  scope :with_user_challenges, -> { includes(:user_challenge) }
+  scope :of_user, ->(user) { with_user_challenges.where(user_challenge: {user_id: user.id}) }
+  scope :group_by_date, -> { group(:date) }
+  scope :with_challenge, -> { includes(:challenge) }
 
   def calculate_category_score
     category = self.user_challenge.challenge.challenge_category
