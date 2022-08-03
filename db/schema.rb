f93801d31e577d5448570718560d4aad40a3d78a
Lisa Yang <lisayang041@gmail.com>
2022-08-03 23:42:24 +0800

    added description column to challenges schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_123601) do
+ActiveRecord::Schema.define(version: 2022_08_03_153717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_123601) do
     t.bigint "challenge_category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["challenge_category_id"], name: "index_challenges_on_challenge_category_id"
   end
 
